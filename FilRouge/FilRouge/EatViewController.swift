4a9881ae91fccd13ee92f2304bc492b770104605
Adrien Delfosse <adri.delfosse@hotmail.com>
2021-06-15 13:55:13 +0200

    feat: add navigation

---
+++ FilRouge/FilRouge/EatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EatViewController.swift
+//  FilRouge
+//
+//  Created by Student05 on 15/06/2021.
+//
+
+import UIKit
+
+class EatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
